6fe74a27c04e3a10dfb43a7d8df5a621ab3bfcf5
Louise Décombe <louise.decombe@laplateforme.io>
2022-07-23 11:38:05 +0200

    add first key components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './components/container/container.component';
+import { MainComponent } from './components/main/main.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    MainComponent,
+    AsideComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
